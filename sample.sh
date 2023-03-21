26af81d964c4d2d0125f68b3bd298a4f7655cbf6
charanporeddy <kumar.charanporeddy@gmail.com>
2023-03-21 22:01:54 +0100

    scripting

---
+++ sample.sh
@@ -0,0 +1,7 @@
+#! bin/bash
+
+x=10
+y=20
+echo "result from script one"
+echo $x "and" $y
+source source
---
+++ script1.sh
@@ -0,0 +1,12 @@
+#! /bin/bash
+
+#Author = Charan
+#Version = bash 1
+#Date = 21/03/2023
+
+echo "The script name is:" $0
+echo "First parameter is:" $1
+echo "Second parameter is" $2
+echo "Script PID is:" $$
+echo "Total Number of arguments:" $#
+
